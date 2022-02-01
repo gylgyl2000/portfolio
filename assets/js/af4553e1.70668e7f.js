60046c36743e3d93239b5376ce50642f5eda361b
gylgyl2000 <gylgyl2000@gmail.com>
2022-02-01 19:53:36 +0100

    Updates

---
+++ assets/js/af4553e1.70668e7f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmon_web_dev_wiki=self.webpackChunkmon_web_dev_wiki||[]).push([[1255],{39082:function(e){e.exports=JSON.parse('{"permalink":"/Mon_Web_Dev_Wiki/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
